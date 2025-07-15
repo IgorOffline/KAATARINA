5acfaab4510811422ae68e3294f2fc5c9510787c
Igor Durbek <igor@igordurbek.com>
2025-07-15 12:02:48 +0200

    Yarn prettier init

--- Kaatarina/src/main/java/igoroffline/practice/kaatarina/KaatarinaApplication.java
+++ Kaatarina/src/main/java/igoroffline/practice/kaatarina/KaatarinaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KaatarinaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KaatarinaApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        SpringApplication.run(KaatarinaApplication.class, args);
+    }
 }
