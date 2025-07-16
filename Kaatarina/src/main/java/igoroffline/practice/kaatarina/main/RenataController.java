6c8dca687edd4d24983b0152b3ce12be0e0af4e0
Igor Durbek <igor@igordurbek.com>
2025-07-16 13:07:32 +0200

    RenataController setup

--- Kaatarina/src/main/java/igoroffline/practice/kaatarina/main/RenataController.java
+++ Kaatarina/src/main/java/igoroffline/practice/kaatarina/main/RenataController.java
@@ -1,4 +1,13 @@
 package igoroffline.practice.kaatarina.main;
 
-// @RestController
-public class RenataController {}
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RenataController {
+
+    @GetMapping("/hello")
+    public String getHelloo() {
+        return "hello";
+    }
+}
